87457178b5d07bcde2a2eaddef4162c46774e5d3
Margarita123rus <margarita23_87@mail.ru>
2020-04-02 00:33:50 +0300

    version 2

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -17,4 +17,27 @@ class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void must2OfferToBuyUpTo1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void must3OfferToBuyUpTo1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(expected, actual);
+    }
 }
+
